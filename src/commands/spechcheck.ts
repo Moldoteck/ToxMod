9a0e286dcb9936156812a5b69491ec6b38fbca60
Cristian Padureac <cpadureac@gmail.com>
2021-04-06 19:34:49 +0300

    more filters

--- src/commands/spechcheck.ts
+++ src/commands/spechcheck.ts
@@ -8,13 +8,17 @@ async function getToxicityResult(language, text) {
   let client = await google.discoverAPI(perspective_link)
   let toxic_score: number = 0
   let profan_score: number = 0
+  let insult_score: number = 0
+  let identity_atack_score: number = 0
   const analyzeRequest = {
     comment: {
       text: text
     },
     requestedAttributes: {
       TOXICITY: { scoreType: "PROBABILITY" },
-      PROFANITY: { scoreType: "PROBABILITY" }
+      PROFANITY: { scoreType: "PROBABILITY" },
+      IDENTITY_ATTACK: { scoreType: "PROBABILITY" },
+      INSULT: { scoreType: "PROBABILITY" }
     },
     languages: [language],
     doNotStore: true
@@ -30,16 +34,19 @@ async function getToxicityResult(language, text) {
   else {
     toxic_score = response.data.attributeScores.TOXICITY.summaryScore.value
     profan_score = response.data.attributeScores.PROFANITY.summaryScore.value
+    insult_score = response.data.attributeScores.INSULT.summaryScore.value
+    identity_atack_score = response.data.attributeScores.IDENTITY_ATTACK.summaryScore.value
+    // console.log(response.data.attributeScores)
   }
-  console.log(toxic_score, profan_score)
-  return [toxic_score, profan_score]
+  console.log(toxic_score, profan_score, insult_score, identity_atack_score)
+  return [toxic_score, profan_score, insult_score, identity_atack_score]
 }
 
 export function checkSpeech(bot: typeof Telegraf) {
   bot.on('text', async ctx => {
     if (ctx.message.text !== undefined) {
       let result = await getToxicityResult(ctx.i18n.t('short_name'), ctx.message.text)
-      if (result[0] > 0.65 || result[1] > 0.8) {
+      if (result[0] > 0.65 || result[1] > 0.7 || result[2] > 0.6 || result[3] > 0.6) {
         ctx.reply(ctx.i18n.t('toxic_notification'), { reply_to_message_id: ctx.message.message_id });
       }
     }
