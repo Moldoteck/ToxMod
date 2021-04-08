36da23deef62457cd123725ab7b2fa7618f60521
Cristian Padureac <cpadureac@gmail.com>
2021-04-08 15:38:44 +0300

    good help message

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,7 +1,11 @@
 const { Telegraf } = require('telegraf')
 
 export function setupHelp(bot: typeof Telegraf) {
-  bot.command(['help', 'start'], (ctx) => {
+  bot.command('start', (ctx) => {
+    ctx.replyWithHTML(ctx.i18n.t('start'))
+  })
+  bot.command('help', (ctx) => {
     ctx.replyWithHTML(ctx.i18n.t('help'))
+
   })
 }
