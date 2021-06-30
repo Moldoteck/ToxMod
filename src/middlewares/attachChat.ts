a5de961757a84cca6946993131b727d4587ab21a
Moldoteck <cpadureac@gmail.com>
2021-06-30 16:17:53 +0300

    chat id fix

--- src/middlewares/attachChat.ts
+++ src/middlewares/attachChat.ts
@@ -2,7 +2,7 @@ import { findChat } from '../models'
 import { Context } from 'telegraf'
 
 export async function attachChat(ctx: Context, next) {
-  if (ctx.from != undefined) {
+  if (ctx.chat != undefined) {
     const dbchat = await findChat(ctx.chat.id)
     ctx.dbchat = dbchat
     return next()
