afeaad2c0cb907c46b33d62a2e26f8237bc40a72
Moldoteck <cpadureac@gmail.com>
2021-07-07 19:54:01 +0300

    chat count

--- src/commands/chatHandler.ts
+++ src/commands/chatHandler.ts
@@ -330,7 +330,23 @@ export function checkSpeech(bot: Telegraf<Context>) {
 
   bot.command('countChats', async (ctx) => {
     if (ctx.message.from.id == 180001222) {
-      ctx.reply(' ' + (await countChats()))
+      let chats = await findAllChats()
+      let users_tot = 0
+      let chat_nr = 0
+      let users_pr = 0
+      for (let element of chats) {
+        console.log(element)
+        try {
+          users_tot += await ctx.telegram.getChatMembersCount(element.id)
+          chat_nr += 1
+        } catch (err) {
+          console.log(err)
+          users_pr += 1
+        }
+      }
+      ctx.reply('Total users ' + users_tot)
+      ctx.reply('Private Users ' + users_pr)
+      ctx.reply('Users ' + chat_nr)
     }
   })
 
