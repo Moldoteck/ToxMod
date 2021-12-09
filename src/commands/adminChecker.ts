ce4e9253b54abf30f1000010af523e768aaa5f1f
Cristian Padureac <Moldoteck@users.noreply.github.com>
2021-12-09 15:46:27 +0200

    New isadmin function

--- src/commands/adminChecker.ts
+++ src/commands/adminChecker.ts
@@ -13,3 +13,19 @@ export async function checkAdmin(ctx) {
   }
   return isAdmin
 }
+
+export async function checkAdminID(ctx, adminID) {
+  let isAdmin = false
+  if (ctx.chat.type == 'group' || ctx.chat.type == 'supergroup') {
+    let chat_admins = await ctx.getChatAdministrators()
+    chat_admins = chat_admins.map(({ user }) => user.id);
+
+    if (chat_admins.indexOf(adminID) != -1) {
+      isAdmin = true
+    }
+  }
+  if (ctx.chat.type == 'private') {
+    isAdmin = true
+  }
+  return isAdmin
+}
